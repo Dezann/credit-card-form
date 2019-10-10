de49e8e42c889f5292aa76ed63c5258340b39e98
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-10-10 08:37:38 +0200

    Add field filling in js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,26 @@
-const cardNumber = document.querySelector("#card-number")
-const name = document.querySelector("#name")
-const expiration = document.querySelector("#expiration")
-const SecurityCode = document.querySelector("#security-code")
+const numberField = document.querySelector("#card-number")
+const nameField = document.querySelector("#name")
+const expirationField = document.querySelector("#expiration")
+const securityCodeField = document.querySelector("#security-code")
 
 const nameInput = document.querySelector("#name-input")
 const numberInput = document.querySelector("#number-input")
 const expirationInput = document.querySelector("#expiration-input")
 const securityCodeInput = document.querySelector("#security-code-input")
 
+let fieldArr = []
 
-nameInput.addEventListener('input', () => {
-    if (nameInput.value.length == 0) {
-        name.innerHTML = 'John Paul';
-    } else {
-        name.innerHTML = nameInput.value;
-    }
-});
+fieldArr.push({ input: nameInput, field: nameField })
+fieldArr.push({ input: numberInput, field: numberField })
+fieldArr.push({ input: expirationInput, field: expirationField })
+fieldArr.push({ input: securityCodeInput, field: securityCodeField })
 
-numberInput.addEventListener('input')
+fieldArr.forEach(el => {
+    el.input.addEventListener('input', () => {
+        if (el.input.value.length == 0) {
+            el.field.innerHTML = 'John Paul';
+        } else {
+            el.field.innerHTML = el.input.value;
+        }
+    })
+})
