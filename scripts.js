857d4b5e009092868d5c2c5c459bd57c9b699e02
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-10-14 08:57:51 +0200

    Add addSpaces function and validateNumberInput function which does not work

--- scripts.js
+++ scripts.js
@@ -10,17 +10,62 @@ const securityCodeInput = document.querySelector("#security-code-input")
 
 let fieldArr = []
 
-fieldArr.push({ input: nameInput, field: nameField, placeholder: "John Paul" })
-fieldArr.push({ input: numberInput, field: numberField, placeholder: "1234 5678 9234 1232" })
-fieldArr.push({ input: expirationInput, field: expirationField, placeholder: "14/23" })
-fieldArr.push({ input: securityCodeInput, field: securityCodeField, placeholder: "2233" })
+fieldArr.push({ name: "nameInput", input: nameInput, field: nameField, placeholder: "John Paul" })
+fieldArr.push({ name: "numberInput", input: numberInput, field: numberField, placeholder: "1234 5678 9234 1232" })
+fieldArr.push({ name: "expirationInput", input: expirationInput, field: expirationField, placeholder: "14/23" })
+fieldArr.push({ name: "securityCodeInput", input: securityCodeInput, field: securityCodeField, placeholder: "2233" })
+
+
+let addSpaces = (input) => {
+    let result = []
+    for (let i = 0; i < input.length; i++) {
+        console.log(i % 4)
+        if (i % 4 === 0) {
+            result.push(' ')
+            console.log("tests")
+        }
+        result.push(input[i])
+    }
+    return result.join("")
+}
+
+let validateNumberInput = (input) => {
+    console.log(typeof (input))
+    console.log(input)
+    console.log(/^[0-9]$/.test(input));
+    if (/^[0-9]$/.test(input)) {
+        return true;
+    }
+    return false;
+}
+
+console.log(validateNumberInput("123 123d"))
+console.log(validateNumberInput("123 123"))
+console.log(validateNumberInput("abc"))
+
+let addSlash = (input) => {
+    let result = []
+    for (let i = 0; i < input.length; i++) {
+        if (i % 2 === 0 && i != 0) {  //because 0%2 === 0 is true
+            result.push("/")
+        }
+        result.push(input[i])
+    }
+    return result.join("")
+}
 
 fieldArr.forEach(el => {
     el.input.addEventListener('input', () => {
-        if (el.input.value.length == 0) {
+        if (el.input.value.length === 0) {
             el.field.innerHTML = el.placeholder || "";
         } else {
-            el.field.innerHTML = el.input.value;
+            if (el.name === "numberInput" && validateNumberInput(el.input.value)) {
+                el.field.innerHTML = addSpaces(el.input.value)
+            }
+            else if (el.name === "expirationInput") {
+                el.field.innerHTML = addSlash(el.input.value)
+            }
+            else { el.field.innerHTML = el.input.value; }
         }
     })
 })
